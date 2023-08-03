7e28f8fe0a71e79847513ea68407c562ab7291b5
Pratyush-Udhani <pratyush.udhani@gmail.com>
2023-08-03 12:49:24 +0530

    changed to app router and initialized trpc)

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,4 @@
-import { exampleRouter } from "~/server/api/routers/example";
+import { router } from "~/server/api/routers/router";
 import { createTRPCRouter } from "~/server/api/trpc";
 
 /**
@@ -7,7 +7,7 @@ import { createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
+  router: router,
 });
 
 // export type definition of API
